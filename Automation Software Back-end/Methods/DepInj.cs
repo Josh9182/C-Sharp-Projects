bef423a93b568ad2a3314f137373ca9af8b19327
Josh Lewis <jlewis200413@gmail.com>
2024-12-13 12:14:25 -0700

    Update DepInj.cs

--- Automation Software Back-end/Methods/DepInj.cs
+++ Automation Software Back-end/Methods/DepInj.cs
@@ -1,11 +1,11 @@
-public class ServiceData { /* Dependency Injection for Controller.cs, responsible for data storage and retrieval */
-    private object _dataStorage; /* Private variable for encapsulation, unable to modify unless internal */
+public class ServiceData { // Dependency Injection for Controller.cs, responsible for data storage and retrieval
+    private object _dataStorage; // Private variable for encapsulation, unable to modify unless internal
 
-    public void SaveData(object data) { /* Method for converting private temp data into global permanent data */
+    public void SaveData(object data) { // Method for converting private temp data into global permanent data
         _dataStorage = data;
     }
 
-    public object ReturnData() { /* Method for returning stored data, finalizing Dependency Injection */
+    public object ReturnData() { // Method for returning stored data, finalizing Dependency Injection
             return _dataStorage;
     }
 }
