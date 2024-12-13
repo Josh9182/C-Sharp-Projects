6a4f25e597c45d53ff39620ab97f6fed8641c18a
Josh Lewis <jlewis200413@gmail.com>
2024-12-13 16:27:33 -0700

    Update Program.cs

--- Automation Software Back-end/Program.cs
+++ Automation Software Back-end/Program.cs
@@ -1,1 +1,12 @@
+var builder = WebApplication.CreateBuilder(args) // builder object, responsible for application configuration with command line argument features
 
+builder.Services.AddSingleton<DataService>(); // Singleton lifetime linking the Dependency Injection class, DataService to the application builder
+builder.Services.AddSingleton<SeleniumContainer>(); // Singleton lifetime linking the Automation class, SeleniumAutomation to the application builder
+
+var app = builder.Build(); // applcation object creation, ready for http requests and routing. Fully configured application. 
+
+app.UseRouting(); // enables middleware, allowing for routing abilities to be accessed and implemented into the application object
+
+app.MapControllers(); // Configures routing to API controllers, links to application object
+
+app.Run();
