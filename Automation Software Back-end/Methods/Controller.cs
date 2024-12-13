1e3637831c34171d2f4143fe6c4a3cc33cfaee33
Josh Lewis <jlewis200413@gmail.com>
2024-12-13 11:08:53 -0700

    Update Controller.cs

--- Automation Software Back-end/Methods/Controller.cs
+++ Automation Software Back-end/Methods/Controller.cs
@@ -2,6 +2,13 @@
 [Route("api/[controller]")] /* Sets API routing to be whatever [controller] is */
 
 public class POSTrequest : ControllerBase { /* Instances ControllerBase for API function inheritance */
+
+    private readonly DataService _dataService; /* Private dataService variable injected via dependency injection from DepInj.cs */
+
+    public POSTrequest(DataService dataService) { /* Constructor instancing the DataService class in regard to dependency injection */
+        _dataService = dataService; /* Converting private temp data into global usable data */
+    }
+
     [HttpPost] /* Sets request type to be POST */
     [Route("fi-data")] /* Sets route ending to be "fi-data", referencing FetchRequest.tsx */
 
@@ -10,6 +17,7 @@ public class POSTrequest : ControllerBase { /* Instances ControllerBase for API 
             return BadRequest(new {CallLog = "Error! Data not found. Please retry."});
         }
         else {
+            _dataService.SaveData(JSONdata); /* Finalizes storage process, saves data and ready for access */
             return Ok(new {CallLog = "Success! Data has been accessed and is now ready for use!"});
         }
     }
