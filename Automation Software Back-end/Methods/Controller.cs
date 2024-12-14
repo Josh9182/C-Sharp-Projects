071c58a97a2847d45d65ca7535128f3209f3cce8
Josh Lewis <jlewis200413@gmail.com>
2024-12-13 20:00:13 -0700

    Update Controller.cs

--- Automation Software Back-end/Methods/Controller.cs
+++ Automation Software Back-end/Methods/Controller.cs
@@ -3,7 +3,7 @@
 
 public class POSTrequest : ControllerBase { // Instances ControllerBase for API function inheritance
 
-    private readonly DataService _dataService; // Private dataService variable injected via dependency injection from DepInj.cs
+    private readonly ServiceData _dataService; // Private dataService variable injected via dependency injection from DepInj.cs
 
     public POSTrequest(DataService dataService) { // Constructor instancing the DataService class in regard to dependency injection
         _dataService = dataService; // Converting private temp data into global usable data
