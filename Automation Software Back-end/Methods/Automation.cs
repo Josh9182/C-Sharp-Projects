a1f5b27e196123d3371f78c94fe593d3edd7f8a1
Josh Lewis <jlewis200413@gmail.com>
2024-12-16 16:43:37 -0700

    Update Automation.cs

--- Automation Software Back-end/Methods/Automation.cs
+++ Automation Software Back-end/Methods/Automation.cs
@@ -5,7 +5,17 @@ using SeleniumExtras.WaitHelpers;
 using OpenQA.Selenium.Interactions;
 
 public class DriverContainer {
-    static void SeleniumDriver() {
+    static void Main(string[] args) { // Endpoint for .NET application
+        IWebDriver driver = new ChromeDriver(); // Instance IWebDriver interface and initiate the ChromeDriver class, opening a Chrome browser
+        driver.Navigate().GoToUrl("https://example.template.com/"); // Navigating the driver to the Chrome URL
+
+        ChromeOptions options = new ChromeOptions(); // Instance ChromeOptions class, allowing for Chrome settings to be altered
+        options.AddArgument("--disable-notifications"); // Disable all notifications that could possibly affect the automation
+        IAlert alert = driver.SwitchTo().Alert(); // Instance the IAlert method, allowing for JavaScript notification manipulation
+        alert.Dismiss(); // Dismiss all notifications
+        
+
+        driver.Quit();
       
     }
 }
