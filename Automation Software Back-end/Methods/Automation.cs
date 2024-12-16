f4cdeb53de5cd6c3b9dc6d00769eb38bbff8675e
Josh Lewis <jlewis200413@gmail.com>
2024-12-16 12:35:27 -0700

    Update Automation.cs

--- Automation Software Back-end/Methods/Automation.cs
+++ Automation Software Back-end/Methods/Automation.cs
@@ -1,3 +1,9 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Interactions;
+
 public class SeleniumContainer {
     static void SeleniumAutomation() {
       
