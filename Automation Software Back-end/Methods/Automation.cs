d9fec31f4ff175061b9d3a2c14b965f444a2d873
Josh Lewis <jlewis200413@gmail.com>
2024-12-24 15:14:48 -0700

    Update Automation.cs

--- Automation Software Back-end/Methods/Automation.cs
+++ Automation Software Back-end/Methods/Automation.cs
@@ -1,40 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
-using System.Threading;
-using System;
-
-public class DriverContainer {
-    static void Main(string[] args) { // Endpoint for .NET application
-        ChromeOptions options = new ChromeOptions(); // Instance ChromeOptions class, allowing for Chrome settings to be altered
-        options.AddArgument("--disable-notifications"); // Disable all notifications that could possibly affect the automation
-        IWebDriver driver = new ChromeDriver(options); // Instance IWebDriver interface and initiate the ChromeDriver class, opening a Chrome browser
-        
-        driver.Navigate().GoToUrl("https://example.template.com/"); // Navigating the driver to the Chrome URL
-
-        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        
-        IAlert alert = driver.SwitchTo().Alert(); // Instance the IAlert method, allowing for JavaScript notification manipulation
-        alert.Dismiss(); // Dismiss all notifications
-
-        IWebElement loginInput = wait.Until(driver => driver.FindElement(By.Id("login_id"))); // Variable that activates the wait instance above combined with the "Until" condition.
-        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", loginInput); // driver instance cast into JavaScript interface for the sake of executing JS code to scroll the source code down until the first argument is found, I.E "loginInput"
-        loginInput.SendKeys("username"); // Type the username into the input element
-        loginInput.SendKeys(Keys.Enter); // Submit element
-
-        IWebElement passInput = wait.Until(driver => driver.FindElement(By.Id("pass_id")));
-        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", passInput);
-        passInput.SendKeys("password");
-        passInput.SendKeys(Keys.Enter);
-
-        driver.Quit();
-    }
-}
-
-
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 using System;
 
 public class DriverContainer { 
